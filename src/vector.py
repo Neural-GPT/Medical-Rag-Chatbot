cf8d27410f6eca2961739a6debc0b946f01ff104
Neural GPT <titanxarestren@gmail.com>
2026-03-26 21:19:33 +0530

    Implement document retrieval and vector store generation
    Add functions to retrieve documents and generate vector store

--- src/vector.py
+++ src/vector.py
@@ -1,1 +1,16 @@
+def retrieve(query, retriever):
+  docs = retriever.invoke(query)
+  return [doc.page_content for doc in docs]
 
+def generate_vector_store(generator, vector_db_path, text, type_):
+  
+  import os
+
+  if os.path.exists(vector_db_path):
+    vector_store = type_.load_local(vector_db_path, generator, allow_dangerous_deserialization = True)
+
+  else:
+    vector_store = type_.from_documents(text, generator)
+    vector_store.save_local(vector_db_path)
+
+  return vector_store
