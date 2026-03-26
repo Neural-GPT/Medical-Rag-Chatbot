1d3194afffa0578142591e386ebf901df323180f
Neural GPT <titanxarestren@gmail.com>
2026-03-26 21:21:28 +0530

    Delete src/split_docs.py

--- src/split_docs.py
+++
@@ -1,6 +0,0 @@
-def split_docs(docs, split, chunk_size, chunk_overlap):
-  splitter = split(chunk_size = chunk_size, chunk_overlap = chunk_overlap, length_function = len)
-  text = splitter.split_documents(docs)
-
-  print(f"Total document chunks created: {len(text)}")
-  return text
