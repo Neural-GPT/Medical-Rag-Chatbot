3c94f3d1b47fb1e1d04e5c39aa5f55056f63ee54
Neural GPT <titanxarestren@gmail.com>
2026-03-26 21:21:40 +0530

    Delete src/load_data.py

--- src/load_data.py
+++
@@ -1,9 +0,0 @@
-def load_data(path, loader):
-
-  import pandas as pd
-  
-  data = pd.read_csv(path)
-  loader = loader(data, page_content_column = "abstract")
-  documents = loader.load()
-
-  return documents
