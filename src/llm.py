8465a915364de63b07b5b468d0883cfef2f24460
Neural GPT <titanxarestren@gmail.com>
2026-03-26 21:17:46 +0530

    Delete src/llm.py

--- src/llm.py
+++
@@ -1,8 +0,0 @@
-def llm(client, prompt, model):
-    response = client.chat.completions.create(
-        model= model,
-        messages=[
-            {"role": "user", "content": prompt}
-        ],
-    )
-    return response.choices[0].message.content
